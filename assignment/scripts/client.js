b6c01253c49127e36b1e5bef9003811bf91b9df5
Cole Dahlen <coledahlen@gmail.com>
2022-12-01 16:43:17 -0600

    The button works, trying to get the rest to work

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -6,6 +6,7 @@ $(document).ready(readyNow);
 
 function readyNow() {
   console.log("DOM is loaded!");
+  $('#addCarButton').on('click', addNewCar)
 
   // add click event handler that calls the addNewCar function
 
@@ -13,16 +14,27 @@ function readyNow() {
 
 function addNewCar() {
   // get input values
-
+  yearInput = $('#yearInput').val();
+  makeInput = $('#makeInput').val();
+  modelInput = $('#modelInput').val();
   // add to array
-
+  garage = {
+    year: Number(yearInput),
+    make: makeInput,
+    model: modelInput
+  }
   // anything else?
-
+  render();
 }
 
 
 
 function render() {
   // update the DOM
-
+  $('.listOfCars').empty();
+  for(let i = 0; i < garage.length; i++){
+    $('.listOfCars').append(`
+      <li>Car Year: ${garage[i].year}. Car Make: ${garage[i].make}. Car Model: ${garage[i].model}
+    `)
+  }
 }
